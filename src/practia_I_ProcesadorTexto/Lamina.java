cdadceb31a83d1de4c35fd822d2aba2ae70bf2fb
CxrlosMX <LuisCRendon131@gmail.com>
2021-06-25 17:06:37 -0500

    Eventos Java

--- src/practia_I_ProcesadorTexto/Lamina.java
+++ src/practia_I_ProcesadorTexto/Lamina.java
@@ -26,9 +26,11 @@ import javax.swing.JTextPane;
  */
 public class Lamina extends JPanel {
 
-    private JMenuItem arial, curiel, verdana;
-    private JMenuItem negrita, cursiva;
-    private JMenuItem chico, mediano, grande, gigante;
+    /*private JMenuItem arial, curiel, verdana;
+     private JMenuItem negrita, cursiva;
+     private JMenuItem chico, mediano, grande, gigante;*/
+    private JMenu estilo, fuente, size;
+    private Font letras;
     private JPanel segundaLamina;
     private JTextPane areaTexto;
 
@@ -37,67 +39,65 @@ public class Lamina extends JPanel {
         segundaLamina = new JPanel();
         JMenuBar mibarra = new JMenuBar();
         //------------------------------------------  
-        JMenu estilo = new JMenu("Estilo");
-        JMenu size = new JMenu("Tamaño");
-        JMenu fuente = new JMenu("fuente");
+        estilo = new JMenu("Estilo");
+        size = new JMenu("Tamaño");
+        fuente = new JMenu("Fuente");
         //------------------------------------------  
+        areaTexto = new JTextPane();
+        //------------------------------------------
+        configuraMenu("Aero", "fuente", "Aero", 9, 10);
+        configuraMenu("Serif", "fuente", "Serif", 9, 10);
+        configuraMenu("Agency FB", "fuente", "Agency FB", 9, 10);
+        //------------------------------------------
+        configuraMenu("Negrita", "estilo", "", Font.BOLD, 1);
+        configuraMenu("Cursiva", "estilo", "", Font.ITALIC, 1);
+        //------------------------------------------
+        configuraMenu("12", "tamaño", "", 9, 12);
+        configuraMenu("16", "tamaño", "", 9, 16);
+        configuraMenu("20", "tamaño", "", 9, 20);
+        configuraMenu("24", "tamaño", "", 9, 24);
+        //------------------------------------------
         mibarra.add(fuente);
         mibarra.add(estilo);
         mibarra.add(size);
-        //------------------------------------------  
-        arial = new JMenuItem("Arial");
-        curiel = new JMenuItem("Aero");
-        verdana = new JMenuItem("Verdana");
-        curiel.addActionListener(new ActionListener() { //Clase interna anonima
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                areaTexto.setFont(new Font("Aero", Font.PLAIN, 12));
-            }
-        });
-        arial.addActionListener(new ActionListener() { //Clase interna anonima
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                areaTexto.setFont(new Font("Arial", Font.PLAIN, 12));
-            }
-        });
-        verdana.addActionListener(new ActionListener() { //Clase interna anonima
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                areaTexto.setFont(new Font("Verdana", Font.PLAIN, 12));
-            }
-        });
-        fuente.add(arial);
-        fuente.add(curiel);
-        fuente.add(verdana);
-        //------------------------------------------  
-        negrita = new JMenuItem("Negrita");
-        cursiva = new JMenuItem("Cursiva");
-        estilo.add(negrita);
-        estilo.add(cursiva);
-        //------------------------------------------  
-        chico = new JMenuItem("12");
-        mediano = new JMenuItem("28");
-        grande = new JMenuItem("25");
-        gigante = new JMenuItem("35");
-        size.add(chico);
-        size.add(mediano);
-        size.add(grande);
-        size.add(gigante);
-        //------------------------------------------  
-
         segundaLamina.add(mibarra);
         add(segundaLamina, BorderLayout.NORTH);
-        //------------------------------------------  
-        areaTexto = new JTextPane();
         add(areaTexto, BorderLayout.CENTER);
     }
 
-    /*private class eventoTexto implements ActionListener {
+    public void configuraMenu(String rotulo, String menu, String tipo_letra, int estilo, int tam) {
 
-     @Override
-     public void actionPerformed(ActionEvent e) {
-     areaTexto.setFont(new Font("Aero", Font.PLAIN, 12));
+        JMenuItem ele_menu = new JMenuItem(rotulo);
+        if (menu.equalsIgnoreCase("fuente")) {
+            this.fuente.add(ele_menu);
+        } else if (menu.equalsIgnoreCase("estilo")) {
+            this.estilo.add(ele_menu);
 
-     }
-     }*/
+        } else {
+            this.size.add(ele_menu);
+        }
+        ele_menu.addActionListener(new eventoTexto(rotulo, tipo_letra, estilo, tam));
+
+    }
+
+    //Clase que gestionara los eventos
+    private class eventoTexto implements ActionListener {
+
+        private String tipo_texto, menu;
+        private int estilo_letra, tamagno;
+
+        public eventoTexto(String menu, String tipo_texto, int estilo_letra, int tamagno) {
+            this.tipo_texto = tipo_texto;
+            this.estilo_letra = estilo_letra;
+            this.tamagno = tamagno;
+            this.menu = menu;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            areaTexto.setFont(new Font(tipo_texto, estilo_letra, tamagno));
+        }
+
+    }
+
 }
