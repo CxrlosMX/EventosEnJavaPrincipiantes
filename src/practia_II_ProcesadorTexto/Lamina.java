c7ba7d8e951b4a607e47b70402d608047b3a0039
CxrlosMX <LuisCRendon131@gmail.com>
2021-06-30 20:52:02 -0500

    Eventos Java

--- src/practia_II_ProcesadorTexto/Lamina.java
+++ src/practia_II_ProcesadorTexto/Lamina.java
@@ -9,11 +9,16 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTextPane;
 import javax.swing.text.StyledEditorKit;
 
@@ -37,6 +42,7 @@ public class Lamina extends JPanel {
     private JPanel terceraLamina;
     private JTextPane areaTexto;
     private JLabel texto;
+    private ButtonGroup grupo;
 
     public Lamina() {
         setLayout(new BorderLayout());
@@ -56,21 +62,66 @@ public class Lamina extends JPanel {
         configuraMenu("Serif", "fuente", "Serif", 9, 10);
         configuraMenu("Agency FB", "fuente", "Agency FB", 9, 10);
         //------------------------------------------
-        configuraMenu("Negrita", "estilo", "", Font.BOLD, 1);
-        configuraMenu("Cursiva", "estilo", "", Font.ITALIC, 1);
+        /*configuraMenu("Negrita", "estilo", "", Font.BOLD, 1);
+         configuraMenu("Cursiva", "estilo", "", Font.ITALIC, 1);*/
+        JCheckBoxMenuItem negrita = new JCheckBoxMenuItem("Negrita", new ImageIcon("src/copiar.gif"));
+        JCheckBoxMenuItem cursiva = new JCheckBoxMenuItem("Cursiva", new ImageIcon("src/copiar.gif"));
+        negrita.addActionListener(new StyledEditorKit.BoldAction());
+        cursiva.addActionListener(new StyledEditorKit.ItalicAction());
+        estilo.add(negrita);
+        estilo.add(cursiva);
         //------------------------------------------
-        configuraMenu("12", "tamaño", "", 9, 12);
-        configuraMenu("16", "tamaño", "", 9, 16);
-        configuraMenu("20", "tamaño", "", 9, 20);
-        configuraMenu("24", "tamaño", "", 9, 24);
+       /* configuraMenu("12", "tamaño", "", 9, 12);
+         configuraMenu("16", "tamaño", "", 9, 16);
+         configuraMenu("20", "tamaño", "", 9, 20);
+         configuraMenu("24", "tamaño", "", 9, 24);*/
+        grupo = new ButtonGroup();
+        agregarRadioButton("12", 12);
+        agregarRadioButton("16", 16);
+        agregarRadioButton("20", 20);
+        agregarRadioButton("24", 24);
+        /* JRadioButtonMenuItem a = new JRadioButtonMenuItem("12");
+         JRadioButtonMenuItem b = new JRadioButtonMenuItem("16");
+         JRadioButtonMenuItem c = new JRadioButtonMenuItem("20");
+         JRadioButtonMenuItem d = new JRadioButtonMenuItem("24");
+         grupo.add(a);
+         grupo.add(b);
+         grupo.add(c);
+         grupo.add(d);
+         a.addActionListener(new StyledEditorKit.FontSizeAction("", 12));
+         b.addActionListener(new StyledEditorKit.FontSizeAction("", 16));
+         c.addActionListener(new StyledEditorKit.FontSizeAction("", 20));
+         d.addActionListener(new StyledEditorKit.FontSizeAction("", 24));
+
+         size.add(a);
+         size.add(b);
+         size.add(c);
+         size.add(d);*/
         //------------------------------------------
         mibarra.add(fuente);
         mibarra.add(estilo);
+
         mibarra.add(size);
         segundaLamina.add(mibarra);
         add(segundaLamina, BorderLayout.NORTH);
         add(terceraLamina, BorderLayout.SOUTH);
         add(areaTexto, BorderLayout.CENTER);
+    }
+
+    private void agregarRadioButton(String texto, int tama) {
+        JRadioButtonMenuItem elemento = new JRadioButtonMenuItem(texto);
+
+        grupo.add(elemento);
+        size.add(elemento);
+        if (texto.equalsIgnoreCase("12")) {
+            elemento.addActionListener(new StyledEditorKit.FontSizeAction("", tama));
+        } else if (texto.equalsIgnoreCase("16")) {
+            elemento.addActionListener(new StyledEditorKit.FontSizeAction("", tama));
+        } else if (texto.equalsIgnoreCase("20")) {
+            elemento.addActionListener(new StyledEditorKit.FontSizeAction("", tama));
+        } else {
+            elemento.addActionListener(new StyledEditorKit.FontSizeAction("", tama));
+        }
     }
 
     public void configuraMenu(String rotulo, String menu, String tipo_letra, int estilo, int tam) {
@@ -85,15 +136,15 @@ public class Lamina extends JPanel {
             } else {
                 ele_menu.addActionListener(new StyledEditorKit.FontFamilyAction("cambia_letra", "Agency FB"));
             }
-        } else if (menu.equalsIgnoreCase("estilo")) {
-            this.estilo.add(ele_menu);
-            if (estilo == Font.BOLD) {
-                ele_menu.addActionListener(new StyledEditorKit.BoldAction());
-
-            } else {
-                ele_menu.addActionListener(new StyledEditorKit.ItalicAction());
-            }
-        } else {
+        } /*else if (menu.equalsIgnoreCase("estilo")) {
+         this.estilo.add(ele_menu);
+         if (estilo == Font.BOLD) {
+         ele_menu.addActionListener(new StyledEditorKit.BoldAction());
+                
+         } else {
+         ele_menu.addActionListener(new StyledEditorKit.ItalicAction());
+         }
+         }*/ else {
             this.size.add(ele_menu);
             ele_menu.addActionListener(new StyledEditorKit.FontSizeAction("", tam));
         }
